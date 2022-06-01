bf9f68895a4b6ef5fab4bec26d3a14df15bcccb2
Robert Szafarczyk <robertszafa@gmail.com>
2022-06-01 16:43:49 +0100

    Add a version of if-else-mul that shows 2x speedup.
    We get the expected 2x speedup when the long branch has a higher
    latency (900 vs 70 cycles), because the 3/5 cycle write-to-read
    pipe latency overhead gets dwarfed. The speeup was only 1.6x if
    the longer branch was 70 cycles.

--- if-else-mul/src/kernel_dynamic.hpp
+++ if-else-mul/src/kernel_dynamic.hpp
@@ -4,15 +4,42 @@
 #include "CL/sycl/queue.hpp"
 #include <CL/sycl.hpp>
 #include <iostream>
+#include <oneapi/dpl/random>
 #include <vector>
 
 #include <sycl/ext/intel/fpga_extensions.hpp>
 
 using namespace sycl;
 
+int long_func(oneapi::dpl::minstd_rand &engine, oneapi::dpl::uniform_int_distribution<int> &distr,
+              const int array_size) {
+  auto rand_0 = distr(engine) % (array_size - 1);
+  auto rand_1 = distr(engine);
+  auto rand_2 = distr(engine);
+  auto rand_3 = distr(engine);
+  return (rand_0 + rand_1 + rand_2 + rand_3) % (array_size - 1);
+}
+
 // Forward declare kernel names to avoid name mangling.
 class CompTrue;
 class SwitchUnit;
+class Read;
+class Write;
+
+// Forward declare pipe class names to avoid name mangling.
+class read_wet_pipe_class;
+class wet_pipe_class;
+class etan_backedge_pipe_class;
+class wet_selector_pipe_class;
+
+class comp_false_predicate_pipe_class;
+class etan_pipe_false_class;
+class etan_pipe_out_false_class;
+
+class comp_true_predicate_pipe_class;
+class wet_pipe_true_class;
+class etan_pipe_true_class;
+class etan_pipe_out_true_class;
 
 template <typename T>
 double if_else_mul_kernel(queue &q, const std::vector<int> &wet, std::vector<T> &B,
@@ -22,76 +49,70 @@ double if_else_mul_kernel(queue &q, const std::vector<int> &wet, std::vector<T> 
   buffer wet_buf(wet);
   buffer B_buf(B);
 
-  using read_wet_pipe = pipe<class p_00, int>;
-  using wet_pipe = pipe<class p_0, int>;
-  using etan_backedge_pipe = pipe<class p_1, T>;
-  using wet_selector_pipe = pipe<class p_8, bool>;
+  using read_wet_pipe = pipe<read_wet_pipe_class, int>;
+  using wet_pipe = pipe<wet_pipe_class, int>;
+  using etan_backedge_pipe = pipe<etan_backedge_pipe_class, T>;
+  using wet_selector_pipe = pipe<wet_selector_pipe_class, bool>;
 
-  using comp_false_predicate_pipe = pipe<class p_10, bool>;
-  using etan_pipe_false = pipe<class p_3, T>;
-  using etan_pipe_out_false = pipe<class p_5, T>;
+  using comp_false_predicate_pipe = pipe<comp_false_predicate_pipe_class, bool>;
+  using etan_pipe_false = pipe<etan_pipe_false_class, T>;
+  using etan_pipe_out_false = pipe<etan_pipe_out_false_class, T>;
 
-  using CompTrue_predicate_pipe = pipe<class p_9, bool>;
-  using wet_pipe_true = pipe<class p_7, int>;
-  using etan_pipe_true = pipe<class p_2, T>;
-  using etan_pipe_out_true = pipe<class p_4, T>;
+  using comp_true_predicate_pipe = pipe<comp_true_predicate_pipe_class, bool>;
+  using wet_pipe_true = pipe<wet_pipe_true_class, int>;
+  using etan_pipe_true = pipe<etan_pipe_true_class, T>;
+  using etan_pipe_out_true = pipe<etan_pipe_out_true_class, T>;
 
   event e = q.submit([&](handler &hnd) {
     accessor B(B_buf, hnd, write_only);
     accessor wet(wet_buf, hnd, read_only);
 
     hnd.single_task<SwitchUnit>([=]() [[intel::kernel_args_restrict]] {
-      T etan = 0.0;
-      bool selector = false;
+      T etan;
 
       for (int i = 0; i < array_size; ++i) {
         int wet_val = wet[i];
         bool wet_predicate = (wet_val > 0);
 
-        if (selector)
-          etan = etan_pipe_out_true::read();
-
+        // Extract only the long latency branch.
+        // A more general approach would be to extract all branches
+        // (if their latency differs), and have a "pick" unit at the end.
         if (wet_predicate) {
-          wet_pipe_true::write(wet_val);
+          comp_true_predicate_pipe::write(true);
           etan_pipe_true::write(etan);
+          wet_pipe_true::write(wet_val);
+          etan = etan_pipe_out_true::read();
         } else {
           etan -= 0.01;
         }
 
-        selector = wet_predicate;
+        B[i] = etan;
       }
 
-      if (selector)
-        etan = etan_pipe_out_true::read();
-
-      B[0] = etan - 2.0;
+      comp_true_predicate_pipe::write(false);
     });
   });
 
   q.submit([&](handler &hnd) {
+    accessor wet(wet_buf, hnd, read_only);
+
     hnd.single_task<CompTrue>([=]() [[intel::kernel_args_restrict]] {
-      while (1) {
-        int wet = wet_pipe_true::read();
+      oneapi::dpl::minstd_rand engine(77, 100);
+      oneapi::dpl::uniform_int_distribution<int> distr;
+
+      // Cannot use "while (1)" if there is preamble before the while block (deadlock).
+      while (comp_true_predicate_pipe::read()) {
         auto etan = etan_pipe_true::read();
-        auto t = 0.25 + etan * T(wet) / 2.0 + exp(etan);
-        etan = etan + t + exp(t + etan);
+        int wet_val = wet_pipe_true::read();
+
+        auto w_i = long_func(engine, distr, array_size);
+        auto t = 0.25 + etan * T(wet_val) / 2.0 + exp(etan);
+        etan += etan + t + exp(t + etan + wet[w_i]);
 
         etan_pipe_out_true::write(etan);
       }
     });
   });
-
-  // q.submit([&](handler &hnd) {
-  //   hnd.single_task<class comp_false>([=]() [[intel::kernel_args_restrict]] {
-  //     // while (comp_false_predicate_pipe::read()) {
-  //     while (1) {
-  //       auto etan = etan_pipe_false::read();
-  //       etan -= 0.01;
-
-  //       etan_pipe_out_false::write(etan);
-  //     }
-  //   });
-  // });
 
   auto start = e.get_profiling_info<info::event_profiling::command_start>();
   auto end = e.get_profiling_info<info::event_profiling::command_end>();
--- if-else-mul/src/kernel_static.hpp
+++ if-else-mul/src/kernel_static.hpp
@@ -3,6 +3,7 @@
 #include "CL/sycl/properties/accessor_properties.hpp"
 #include <CL/sycl.hpp>
 #include <iostream>
+#include <oneapi/dpl/random>
 #include <vector>
 
 #include <sycl/ext/intel/fpga_extensions.hpp>
@@ -11,6 +12,15 @@ using namespace sycl;
 
 // Forward declare kernel names to avoid name mangling.
 class Compute;
+
+int long_func(oneapi::dpl::minstd_rand &engine, oneapi::dpl::uniform_int_distribution<int> &distr,
+              const int array_size) {
+  auto rand_0 = distr(engine) % (array_size - 1);
+  auto rand_1 = distr(engine);
+  auto rand_2 = distr(engine);
+  auto rand_3 = distr(engine);
+  return (rand_0 + rand_1 + rand_2 + rand_3) % (array_size - 1);
+}
 
 template <typename T>
 double if_else_mul_kernel(queue &q, const std::vector<int> &wet, std::vector<T> &B,
@@ -24,21 +34,24 @@ double if_else_mul_kernel(queue &q, const std::vector<int> &wet, std::vector<T> 
     accessor wet(wet_buf, hnd, read_only);
     accessor B(B_buf, hnd, write_only);
 
-    hnd.single_task<Compute>([=]() [[intel::kernel_args_restrict]]  {
+    hnd.single_task<Compute>([=]() [[intel::kernel_args_restrict]] {
+      oneapi::dpl::minstd_rand engine(77, 100);
+      oneapi::dpl::uniform_int_distribution<int> distr;
+
       T etan, t = 0.0;
-      // II=78
       for (int i = 0; i < array_size; ++i) {
+        // RAW depenendency accross iterations for etan and engine.
         if (wet[i] > 0) {
-          // 78 cycles of stall for 32-bit (more for 64-bit since can't use DSPs)
           t = 0.25 + etan * T(wet[i]) / 2.0 + exp(etan);
-          etan = etan + t + exp(t+etan);
+          auto t = 0.25 + etan * T(wet[i]) / 2.0 + exp(etan);
+          auto w_i = long_func(engine, distr, array_size);
+          etan += etan + t + exp(t + etan + wet[w_i]);
         } else {
-          // 3 cycles of stall
           etan -= 0.01;
         }
-      }
 
-      B[0] = etan - 2.0;
+        B[i] = etan;
+      }
     });
   });
 
