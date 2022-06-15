09a96efeb1b6be25bac3bfb1f501ea44b4f1a6a9
Robert Szafarczyk <robertszafa@gmail.com>
2022-06-15 17:44:42 +0100

    cp: ~2x perf on small a_sizes, res not correct.
    Deadlocks for higher a_sizes.

--- histogram/src/kernel_dynamic.hpp
+++ histogram/src/kernel_dynamic.hpp
@@ -1,11 +1,9 @@
-
 #include "CL/sycl/access/access.hpp"
 #include "CL/sycl/builtins.hpp"
 #include "CL/sycl/properties/accessor_properties.hpp"
 #include <CL/sycl.hpp>
 #include <iostream>
 #include <vector>
-#include <utility>
 
 #include <sycl/ext/intel/fpga_extensions.hpp>
 
@@ -15,22 +13,20 @@ using PipelinedLSU = ext::intel::lsu<>;
 using BurstCoalescedLSU = ext::intel::lsu<ext::intel::burst_coalesce<false>, 
                                           ext::intel::statically_coalesce<false>>;
 
-// This should be STORE_Q_SIZE >= STORE_LATENCY
 constexpr uint STORE_Q_SIZE = Q_SIZE;
 constexpr uint STORE_LATENCY = 72;
 
 struct store_entry {
-  uint idx; // This should be the full address in a real impl.
+  int idx; // This should be the full address in a real impl.
   uint val;
   bool executed;
   int countdown;
 };
 
-struct m_store_entry {
-  uint idx_hist;
-  uint idx_store_q;
+struct pair {
+  int idx_q; 
+  uint idx_hist; 
 };
-
 
 
 double histogram_kernel(queue &q, const std::vector<uint> &feature, const std::vector<uint> &weight,
@@ -44,9 +40,9 @@ double histogram_kernel(queue &q, const std::vector<uint> &feature, const std::v
   buffer hist_buf(hist);
 
   using weight_load_pipe = pipe<class weight_load_pipe_class, uint, 16>;
-  using m_load_pipe = pipe<class m_load_pipe_class, uint, 16>;
-  using m_store_pipe = pipe<class m_store_pipe_class, uint, 16>;
-  using hist_load_pipe = pipe<class hist_load_pipe_class, uint, 16>;
+  using feature_load_pipe = pipe<class feature_load_pipe_class, uint, 16>;
+  using feature_store_pipe = pipe<class feature_store_pipe_class, uint, 16>;
+  using hist_load_pipe = pipe<class hist_load_pipe_class, uint, 32>;
 
   using hist_store_pipe = pipe<class hist_store_pipe_class, uint>;
 
@@ -63,166 +59,143 @@ double histogram_kernel(queue &q, const std::vector<uint> &feature, const std::v
   
   q.submit([&](handler &hnd) {
     accessor feature(feature_buf, hnd, read_only);
-
     hnd.single_task<class LoadFeature>([=]() [[intel::kernel_args_restrict]] {
-      for (int i = 0; i < array_size; ++i) {
-        uint m = feature[i];
-        m_load_pipe::write(m);
-      }
+      for (int i = 0; i < array_size; ++i) 
+        feature_load_pipe::write(feature[i]);
     });
   });
-
   q.submit([&](handler &hnd) {
     accessor feature(feature_buf, hnd, read_only);
-
     hnd.single_task<class LoadFeature2>([=]() [[intel::kernel_args_restrict]] {
-      for (int i = 0; i < array_size; ++i) {
-        uint m = feature[i];
-        m_store_pipe::write(m);
-      }
+      for (int i = 0; i < array_size; ++i) 
+        feature_store_pipe::write(feature[i]);
     });
   });
  
+
   q.submit([&](handler &hnd) {
     accessor hist(hist_buf, hnd, read_write);
-    // accessor feature(feature_buf, hnd, read_only);
 
     hnd.single_task<class LoadStoreHist>([=]() [[intel::kernel_args_restrict]] {
       [[intel::fpga_register]] store_entry store_q[STORE_Q_SIZE];
-
-      // constexpr uint M_STORE_Q_SIZE = 16;
-      // Keeps track of m_store into hist, and free_slot into store_q
-      // [[intel::fpga_register]] m_store_entry m_store_q[M_STORE_Q_SIZE];
+      
+      // A FIFO of {idx_into_store_q, idx_into_hist} pairs.
+      constexpr uint IDX_FIFO_SIZE = STORE_Q_SIZE;
+      [[intel::fpga_register]] pair store_idx_fifo[IDX_FIFO_SIZE];
+      uint store_idx_fifo_head = 0;
 
       // All entries are invalid at the start.
       #pragma unroll
       for (uint i=0; i<STORE_Q_SIZE; ++i) {
-        store_q[i].idx = (uint) -1;
-        store_q[i].countdown = -1;
-        store_q[i].executed = false;
+        store_q[i] = {-1, 0, false, -1};
       }
 
+      uint i_store_vals = 0;
+      uint i_store_idxs = 0;
       uint i_load = 0;
-      uint i_store = 0;
-      uint enqued_idx_counter = 0;
-      uint enqued_val_counter = 0;
 
-      bool is_load_waiting_for_executed = false;
-      uint m_load;
+      // Is load written in the client pipe?
+      bool load_returned = true;
+      uint load_val;
 
       [[intel::ivdep]]
-      // for (int i_top = 0; i_top < array_size; ++i_top) {
-      while (i_store < array_size) {
-        sycl::ext::oneapi::experimental::printf("New iter i_load %d   i_store %d\n", i_load, i_store);
+      while (i_store_vals < array_size) {
+        
+        // sycl::ext::oneapi::experimental::printf("\n -- Iter --\n");
 
-        if (i_load < array_size) {
-          if (!is_load_waiting_for_executed)
-            m_load = m_load_pipe::read();
+        /* Load logic */
+        if (i_load < array_size && load_returned) {
+          bool load_idx_pipe_succ = false;
+          auto load_idx = feature_load_pipe::read(load_idx_pipe_succ);
 
-          bool forward = false;
-          uint hist_val;
+          if (load_idx_pipe_succ) {
+            bool forward = false;
 
-          // TODO: Do we need to explicitly pick the entry with the hisghest idx (latest store)? 
-          #pragma unroll
-          for (uint i=0; i<STORE_Q_SIZE; ++i) {
-            if (store_q[i].executed && store_q[i].idx == ext::intel::fpga_reg(m_load)) {
-              forward = true;
-              hist_val = store_q[i].val;
-              is_load_waiting_for_executed = false;
+            // TODO: Handle case where idx matches but store value is not yet available!
+            #pragma unroll
+            for (uint i=0; i<STORE_Q_SIZE; ++i) {
+              if (store_q[i].idx == load_idx && store_q[i].executed) {
+                forward = true;
+                load_val = store_q[i].val;
+              }
             }
-            else if (!store_q[i].executed && store_q[i].idx == ext::intel::fpga_reg(m_load)) {
-              is_load_waiting_for_executed |= true;
-            }
-            else {
-              is_load_waiting_for_executed |= false;
-            }
-          }
-
-          if (!forward || !is_load_waiting_for_executed) {
-            is_load_waiting_for_executed = false;
 
             if (!forward) {
-              hist_val = PipelinedLSU::load(hist.get_pointer() + m_load);
-              // sycl::ext::oneapi::experimental::printf("Load %d\n", m_load);
+              load_val = PipelinedLSU::load(hist.get_pointer() + load_idx);
             }
-            
-            hist_load_pipe::write(hist_val);
-
-            i_load++;
+            load_returned = false;
           }
         }
-        
-        bool to_shift = false;
-        // Countdown store latency for executed entries
+        if (i_load < array_size && !load_returned) {
+          // Keep trying to return load once on every iteration, until successful.
+          hist_load_pipe::write(load_val, load_returned);
+          if (load_returned) {
+            i_load += 1;
+            // sycl::ext::oneapi::experimental::printf("Written to compute %d\n", load_val);
+          }
+        }
+        /* End Load logic */
+
+        /* Store logic */
+        // Countdown stores in progress.
+        int next_q_slot = -1;
         #pragma unroll
         for (uint i=0; i<STORE_Q_SIZE; ++i) {
           if (store_q[i].executed) {
             store_q[i].countdown -= 1;
-            if (store_q[i].countdown <= 0) {
-              to_shift |= true;
-            }
+          }
+
+          if (store_q[i].countdown <= 0) {
+            next_q_slot = i;
+            store_q[i] = {-1, 0, false, -1};
           }
         }
+        // sycl::ext::oneapi::experimental::printf("next_slot %d\n", next_q_slot);
 
-        sycl::ext::oneapi::experimental::printf("Countdown q[0]: %d\n", store_q[0].countdown);
-        sycl::ext::oneapi::experimental::printf("Executed q[0]: %d\n", store_q[0].executed);
-        if (to_shift) {
-          // Shift store queue.
+        // Non blocking store_idx read.
+        uint store_idx;
+        bool store_idx_pipe_succ = false;
+        if (next_q_slot != -1 && store_idx_fifo_head < IDX_FIFO_SIZE) {
+          store_idx = feature_store_pipe::read(store_idx_pipe_succ);
+        }
+
+        if (store_idx_pipe_succ) {
+          // sycl::ext::oneapi::experimental::printf("Next store_idx %d\n", store_idx);
+          store_q[next_q_slot].idx = store_idx;
+          store_q[next_q_slot].countdown = STORE_LATENCY;
+
+          store_idx_fifo[store_idx_fifo_head] = {next_q_slot, store_idx};
+          store_idx_fifo_head += 1;
+
+          i_store_idxs += 1;
+        }
+
+        // Non blocking store_val read.
+        bool store_val_pipe_succ = false;
+
+        uint store_val ;
+        if (i_store_idxs > i_store_vals) {
+          store_val = hist_store_pipe::read(store_val_pipe_succ);
+        }
+        
+        if (store_val_pipe_succ) {
+          auto q_idx_hist_idx_pair = store_idx_fifo[0];
+
+          // sycl::ext::oneapi::experimental::printf("Stored hist[ %d ]\n", q_idx_hist_idx_pair.idx_hist);
+          // sycl::ext::oneapi::experimental::printf("Q slot %d\n", q_idx_hist_idx_pair.idx_q);
+          hist[q_idx_hist_idx_pair.idx_hist] = store_val;
+          store_q[q_idx_hist_idx_pair.idx_q].executed = true;
+
           #pragma unroll
-          for (uint i = 1; i < STORE_Q_SIZE; ++i) {
-            store_q[i - 1] = store_q[i];
+          for (uint i=1; i<IDX_FIFO_SIZE; ++i) {
+            store_idx_fifo[i-1] = store_idx_fifo[i];
           }
 
-          enqued_idx_counter -= 1;
-          enqued_val_counter -= 1;
-          store_q[enqued_idx_counter] = store_entry{(uint) -1, 0, false, -1};
+          store_idx_fifo_head -= 1;
+          i_store_vals += 1;
         }
-
-        bool m_store_pipe_succ = false;
-        uint m_store;
-        if (enqued_idx_counter < STORE_Q_SIZE) {
-          m_store = m_store_pipe::read(m_store_pipe_succ);
-        }
-        if (m_store_pipe_succ) {
-          // Enque the index in the store queue.
-          store_q[enqued_idx_counter].idx = m_store;
-          store_q[enqued_idx_counter].countdown = STORE_LATENCY;
-          store_q[enqued_idx_counter].executed = false;
-          
-          sycl::ext::oneapi::experimental::printf("Store 1: %d\n", m_store);
-
-          // Index FIFO to store the value in the correct slot once it's is ready.
-          // store_q[num_enqued_stores].idx_hist = m_store;
-          // store_q[num_enqued_stores].idx_store_q = free_slot;
-          enqued_idx_counter++;
-        }
-
-        if (enqued_idx_counter > 0) {
-          bool store_pipe_succ = false;
-          auto new_hist = hist_store_pipe::read(store_pipe_succ);
-
-          sycl::ext::oneapi::experimental::printf("read store_pipe_succ: %d\n", store_pipe_succ);
-          if (store_pipe_succ) {
-            auto next_m_to_store = store_q[enqued_val_counter];
-
-            hist[next_m_to_store.idx] = new_hist;
-            store_q[enqued_val_counter].val = new_hist; 
-            store_q[enqued_val_counter].executed = true;
-            i_store++;
-            enqued_val_counter++;
-
-            sycl::ext::oneapi::experimental::printf("Store 2: %d\n", next_m_to_store.idx);
-            // // Shift FIFO.
-            // #pragma unroll
-            // for (uint i=0; i<M_STORE_Q_SIZE-1; ++i) {
-            //   m_store_q[0] = m_store_q[i + 1];
-            // }
-            // num_enqued_stores -= 1;
-          }
-        }
-      
+        /* End Store logic */
       }
-
     });
   });
 
