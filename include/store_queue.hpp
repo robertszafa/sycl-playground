93a37cf2f1ae68b459ad81cffd0105b81df04d3f
Robert Szafarczyk <robertszafa@gmail.com>
2022-08-04 14:09:39 +0100

    cp: Tags as template n-tuples (has some bug).
    Keeping for record before switching back to single int tags.

--- include/store_queue.hpp
+++ include/store_queue.hpp
@@ -39,19 +39,40 @@ using PipelinedLSU = ext::intel::lsu<>;
 // Forward declaration to avoid name mangling.
 class StoreQueueKernel;
 
-template <typename ld_idx_pipes, typename ld_val_pipes, int num_lds, typename T_idx_tag_pair,
-          typename st_idx_pipe, typename st_val_pipe, typename end_signal_pipe, 
-          bool FORWARD=true, uint QUEUE_SIZE=8, uint ST_LATENCY=12, typename T_val>
+
+template <typename ld_idx_pipes, typename ld_val_pipes, int num_lds, typename idx_tag_pair_t, 
+          typename st_idx_pipe, typename st_val_pipe, typename end_signal_pipe, typename base_ld_tags,
+          int base_st_tag, int NUM_TAGS, bool FORWARD=true, int QUEUE_SIZE=8, int ST_LATENCY=12, 
+          typename T_val>
 void StoreQueue(queue &q, buffer<T_val> &data_buf) {
-  struct store_entry {
-    int idx; // This should be the full address in a real impl.
-    int tag;
-    bool waiting_for_val;
-    int8_t countdown;
-  };
 
   constexpr int kQueueLoopIterBitSize = fpga_tools::BitsForMaxValue<QUEUE_SIZE+1>();
   using storeq_idx_t = ac_int<kQueueLoopIterBitSize, false>;
+
+  // Extract the tag type (usually some kind of n-tuple with a comp. operator).
+  using tag_t = NTuple<int, NUM_TAGS>; // decltype(idx_tag_pair_t{}.second);
+  /// One less than lowest loop iter value .
+  tag_t base_tag;
+  UnrolledLoop<NUM_TAGS>([&](auto k) { base_tag. template get<k>() = -1; } );
+  // auto& kNumTags = idx_tag_pair_t{}.second.NumTys;
+
+  auto is_tag_younger = [&] (tag_t& first, tag_t& second) {
+    bool is_younger = first. template get<0>() <= second. template get<0>();
+    UnrolledLoop<1, NUM_TAGS-1>([&](auto j) {
+      is_younger = is_younger && (first. template get<j>() < second. template get<j>());
+    });
+
+    if constexpr (NUM_TAGS == 1) return is_younger;
+
+    return is_younger && (first. template get<NUM_TAGS-1>() < second. template get<NUM_TAGS-1>());
+  };
+
+  struct store_entry {
+    int idx; // This should be the full address in a real impl.
+    tag_t tag;
+    bool waiting_for_val;
+    int8_t countdown;
+  };
 
   q.submit([&](handler &hnd) {
     accessor data(data_buf, hnd, read_write);
@@ -59,10 +80,11 @@ void StoreQueue(queue &q, buffer<T_val> &data_buf) {
     hnd.single_task<StoreQueueKernel>([=]() [[intel::kernel_args_restrict]] {
       [[intel::fpga_register]] store_entry store_entries[QUEUE_SIZE];
       [[intel::fpga_register]] T_val store_entries_val[QUEUE_SIZE];
+      // [[intel::fpga_register]] tag_t store_entries_tag[QUEUE_SIZE];
 
       #pragma unroll
       for (uint i=0; i<QUEUE_SIZE; ++i) {
-        store_entries[i] = {-1, -2, 0, 0};
+        store_entries[i] = {-1}; //, base_tag, 0, 0};
       }
 
       // How many store (valid) indexes were read from st_idx pipe.
@@ -73,21 +95,26 @@ void StoreQueue(queue &q, buffer<T_val> &data_buf) {
       storeq_idx_t stq_tail = 0;
       storeq_idx_t stq_head = 0;
 
-      int tag_store = -1;
+      tag_t tag_store;
+      tag_store. template get<0>() = -1;
+      tag_store. template get<1>() = 1;
+
       int idx_store;
       T_val val_store;
-      T_idx_tag_pair idx_tag_pair_store;
+      idx_tag_pair_t idx_tag_pair_store;
 
       NTuple<T_val, num_lds> val_load_tp;
       NTuple<int, num_lds> idx_load_tp;
-      NTuple<int, num_lds> tag_load_tp;
+      NTuple<tag_t, num_lds> tag_load_tp;
       NTuple<bool, num_lds> consumer_load_succ_tp;
       NTuple<bool, num_lds> is_load_waiting_tp;
-      NTuple<T_idx_tag_pair, num_lds> idx_tag_pair_load_tp;
+      NTuple<idx_tag_pair_t, num_lds> idx_tag_pair_load_tp;
 
       UnrolledLoop<num_lds>([&](auto k) {
         consumer_load_succ_tp. template get<k>() = true;
-        tag_load_tp. template get<k>() = -1;
+        // tag_load_tp. template get<k>() = base_tag;
+        tag_load_tp. template get<k>(). template get<0>() = -1;
+        tag_load_tp. template get<k>(). template get<1>() = 0;
         is_load_waiting_tp. template get<k>() = false;
       });
 
@@ -97,6 +124,8 @@ void StoreQueue(queue &q, buffer<T_val> &data_buf) {
       while (!end_signal || (i_store_idx > i_store_val)) {
         /* Start Load  Logic */
         // All loads can proceed in parallel. The below unrolls the template pipe array. 
+        // PRINTF("tag_load_tp.0 %d  tag_load_tp.1 %d\n", std::get<0>(tag_load_tp. template get<0>()), 
+        //                                              std::get<1>(tag_load_tp. template get<0>()));
         UnrolledLoop<num_lds>([&](auto k) {
           // Use shorter names.
           auto& val_load = val_load_tp. template get<k>();
@@ -106,7 +135,7 @@ void StoreQueue(queue &q, buffer<T_val> &data_buf) {
           auto& is_load_waiting = is_load_waiting_tp. template get<k>();
           auto& idx_tag_pair_load = idx_tag_pair_load_tp. template get<k>();
 
-          if ((tag_load <= tag_store && consumer_load_succ) || is_load_waiting) {
+          if (is_load_waiting || (is_tag_younger(tag_load, tag_store) && consumer_load_succ)) {
             // Check for new ld requests.
             bool idx_load_pipe_succ = false;
             if (!is_load_waiting) {
@@ -123,29 +152,34 @@ void StoreQueue(queue &q, buffer<T_val> &data_buf) {
               if constexpr (FORWARD) {
                 // If found, make sure it's the youngest store occuring before this ld
                 // by finding the store with the max_tag, such that max_tag < this_ld_tag
-                int max_tag = -1; 
+                tag_t max_tag = base_tag; 
+                bool is_match = false;
                 #pragma unroll
                 for (storeq_idx_t i=0; i<QUEUE_SIZE; ++i) {
-                  auto st_entry = store_entries[i];
-                  if (st_entry.idx == idx_load && st_entry.tag < tag_load && st_entry.tag > max_tag) {
-                    is_load_waiting = st_entry.waiting_for_val;
+                  auto st_entry_tag = store_entries[i].tag;
+                  const bool entry_before_ld = is_tag_younger(st_entry_tag, tag_load); 
+                  const bool entry_youngest = is_tag_younger(max_tag, st_entry_tag);
+                  if (store_entries[i].idx == idx_load && entry_before_ld && entry_youngest) {
+                    is_match = true;
+                    is_load_waiting = store_entries[i].waiting_for_val;
                     val_load = store_entries_val[i];
-                    max_tag = st_entry.tag;
+                    max_tag = st_entry_tag;
                   }
                 }
 
-                if (!is_load_waiting && max_tag == -1) {
+                if (!is_load_waiting && is_match) {
                   // Not found in store_q, so issue ld.
                   val_load = PipelinedLSU::load(data.get_pointer() + idx_load);
                 }
               }
               else {
-                int max_tag = -1; 
+                tag_t max_tag = base_tag; 
                 #pragma unroll
                 for (storeq_idx_t i=0; i<QUEUE_SIZE; ++i) {
-                  auto st_entry = store_entries[i];
-                  if (st_entry.idx == idx_load && st_entry.tag < tag_load) {
-                    is_load_waiting = true;
+                  if (store_entries[i].idx == idx_load) {
+                    if (is_tag_younger(store_entries[i].tag, tag_load)) {
+                      is_load_waiting = true;
+                    }
                   }
                 }
 
@@ -196,6 +230,7 @@ void StoreQueue(queue &q, buffer<T_val> &data_buf) {
             if (idx_store != -1) {
 
               store_entries[stq_head] = {idx_store, tag_store, true};
+              // store_entries_tag[stq_head] = tag_store;
 
               i_store_idx++;
               stq_head = (stq_head+1) % QUEUE_SIZE;
